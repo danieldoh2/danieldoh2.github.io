a5d5f2da9205cb64be97cda84a96e7ec05fb7f44
danieldoh2 <danieldoh.inbox@gmail.com>
2023-01-06 16:17:37 -0600

    newchanges

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,103 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  "imagebaseurl": "https://rbhatia46.github.io/",
+  "name": "Daniel Doh",
+  "role": "Software Developer/ Data Science Undergraduate",
+  "linkedinId": "Your LinkedIn Id",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/daniel-doh1/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/danieldoh2",
+      "className": "fa fa-github"
+    }
+  ],
+  "aboutme": "I am currently a Boston University sophomore pursuing my B.S. in Data Science. I am a self-taught Web/IOS Developer, presently looking for opportunities for product management, particularly within education-technology. I believe that a fulfilling life can be achieved by exploring new hobbies. Outside of class, I am currently learning how to play piano, box, and speak/write Korean.",
+  "address": "Boston, Massachusetts",
+  "website": "https://rbhatia46.github.io",
+  "education": [
+    {
+      "UniversityName": "Boston University",
+      // "specialization": "Data Science",
+      "MonthOfPassing": "May",
+      "MonthOfStarting": "September",
+      "YearOfStarting": "2021",
+      "YearOfPassing": "2025"
+    },
+    {
+      "UniversityName": "Rolling Meadows High School",
+      "MonthOfPassing": "May",
+      "MonthOfStarting": "August",
+      "YearOfStarting": "2017",
+      "YearOfPassing": "2021"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "1ounce",
+      "specialization": "Software Developer",
+      "MonthOfPassing": "Current",
+      "MonthOfStarting": "October",
+      "YearOfStarting": "2022",
+      // "YearOfPassing": "2023"
+      // "Achievements": "Some Achievements"
+    },
+    {
+      "CompanyName": "Berea College Information Systems and Services",
+      "specialization": "IT Help Desk Technician",
+      "MonthOfPassing": "June",
+      "MonthOfStarting": "January",
+      "YearOfStarting": "2022",
+      "YearOfPassing": "2022"
+      // "Achievements": "Some Achievements"
+    }
+  ],
+  "skills": [
+    {
+      "skillname": "Data Analysis"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "Machine Learning"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
